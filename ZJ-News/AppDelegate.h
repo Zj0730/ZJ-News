ec71eb726d3685c96f7d3a641b8a8a155a0749a1
teacher <teacher@teacherdeMac.local>
2016-11-09 14:29:52 +0800

    Initial Commit

---
+++ ZJ-News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJ-News
+//
+//  Created by teacher on 16-11-9.
+//  Copyright (c) 2016年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJ-News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJ-News
+//
+//  Created by teacher on 16-11-9.
+//  Copyright (c) 2016年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
